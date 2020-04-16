8a577abfb049f03a3bccba0ad8f45b30f186c30e
syn_adm365 <syn_adm365@outlook.com>
2020-04-16 10:53:35 +0530

    First commit

---
+++ bulkybook/bulkybook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bulkybook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
